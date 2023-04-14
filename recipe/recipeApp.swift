48d232eb705628debbed4585f781509fd2cd7179
axel beke <axel@beke.se>
2023-04-14 12:21:23 +0200

    Initial Commit

---
+++ recipe/recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeApp.swift
+//  recipe
+//
+//  Created by Axel Beke on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
